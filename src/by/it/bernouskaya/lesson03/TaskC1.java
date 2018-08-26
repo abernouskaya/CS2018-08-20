b6ec5b0331fdc5533f4bea429278223597013d06
abernouskaya <abernouskaya@mail.ru>
2018-08-26 18:00:46 +0300

    lesson03

--- src/by/it/bernouskaya/lesson03/TaskC1.java
+++ src/by/it/bernouskaya/lesson03/TaskC1.java
@@ -24,14 +24,14 @@ TC = (TF – 32) * 5/9
 в градусы Фаренгейта и возвращать это число.*/
 
 
+import java.util.Scanner;
+
 class TaskC1 {
-
-
-
 
 
 //    public static void main(String[] args) {
 //        System.out.println(convertCelsiumToFahrenheit(40));
 //    }
+
 
 }
