02b956138fd18693530621d560eb6c93534b0efd
abernouskaya <abernouskaya@mail.ru>
2018-08-30 15:14:35 +0300

    lesson05

--- src/by/it/bernouskaya/lesson05/TaskC1.java
+++ src/by/it/bernouskaya/lesson05/TaskC1.java
@@ -14,23 +14,40 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+
 import java.util.List;
 
 public class TaskC1 {
-    public static void main(String[] args) throws Exception
-    {
-        BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<Integer> list= new ArrayList<Integer>(20);
-        Collections.addAll(list);
+    public static void main(String[] args) throws Exception {
 
 
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList <Integer>list = new ArrayList<>();
+        ArrayList <Integer>a = new ArrayList<>();
+        ArrayList <Integer>b = new ArrayList<>();
+        ArrayList <Integer>c = new ArrayList<>();
 
 
+        while (true) {
+            String s = reader.readLine();
+            if (s.isEmpty()) break;
+
+
+            int x = Integer.parseInt(s);
+            if (x % 3 == 0)
+                list.add(x);
+
+            if (x % 2 == 0)
+                a.add(x);
+
+            if (x % 3 != 0 && x % 2 != 0)
+                b.add(x);
+            if (x%3==0 && x%2==0)
+                c.add(x);
+
+             b.addAll(c);}
+
+
+        }
     }
 
-    private static void printList(List<Integer> list) {
-        for (Integer aList : list) System.out.println(aList);
-    }
-
-
-}
--- src/by/it/bernouskaya/lesson05/TaskC2.java
+++ src/by/it/bernouskaya/lesson05/TaskC2.java
@@ -4,15 +4,42 @@ package by.it.bernouskaya.lesson05;
 Задача: Написать программу, которая вводит с клавиатуры 20 чисел и выводит их в убывающем порядке.
 */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class TaskC2 {
     //Напишите тут ваш код
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> list =new ArrayList<>();
+        while (true){
+            String s=reader.readLine();
+            if (s.isEmpty()) break;
+            list.add(Integer.parseInt(s));
+
+        }
+
+        String[] array = list.toArray(new String[list.size()]);
+        sort(array);
+        for (String x: array){
+
+            System.out.println(x);
+        }
 
 
-
-
-    public static void sort(int[] array) {
-        //Напишите тут ваш код
     }
 
+
+
+    public static void sort(String[] array) {
+        //Напишите тут ваш код
+
+        for (int i = 1; i < array.length - i + 1; i++) {
+
+            array[i - 1] = array[i];
+        }
+    }
 }
